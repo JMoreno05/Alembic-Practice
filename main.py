c5dfe43b37555e156b4a5a43509d8804d63be74a
JMoreno05 <57776551+JMoreno05@users.noreply.github.com>
2024-11-18 15:35:59 -0500

    parameters video

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from enum import Enum
+
 app = FastAPI()
 
+class GenreURLChoices(Enum):
+    METAL = 'metal'
+    ROCK = 'rock'
+    COUNTRY = 'country'
+    POP = 'pop'
+    RNB = 'rnb'
+    COMEDY = 'comedy'
+
 BANDS = [
-    {'id': 1, 'name': 'The Beatles', 'genre': 'Rock'},
+    {'id': 1, 'name': 'Black Sabbath', 'genre': 'Metal'},
     {'id': 2, 'name': 'Led Zeppelin', 'genre': 'Rock'},
-    {'id': 3, 'name': 'The Rolling Stones', 'genre': 'Rock'},
-    {'id': 4, 'name': 'The Who', 'genre': 'Rock'},
-    {'id': 5, 'name': 'Pink Floyd', 'genre': 'Rock'},
+    {'id': 3, 'name': 'Dixie Chicks', 'genre': 'Country'},
+    {'id': 4, 'name': 'Pink', 'genre': 'Pop'},
+    {'id': 5, 'name': 'The Supremes', 'genre': 'R&B'},
+    {'id': 6, 'name': 'Bill Engvall', 'genre': 'Comedy'},
 ]
 
 @app.get('/')
@@ -16,7 +27,20 @@ async def index() -> dict[str,str]:
 @app.get('about')
 async def about() -> str:
     return 'About'
-
+ 
 @app.get('/bands')
 async def get_bands() -> list[dict]:
     return BANDS
+
+@app.get('/bands/{band_id}')
+async def get_band(band_id: int) -> dict:
+    band = next((band for band in BANDS if band['id'] == band_id), None)
+    if band is None:
+        # status code 404
+        raise HTTPException(status_code=404, detail='Band not found')
+    return band
+
+@app.get('/bands/genre/{genre}')
+async def get_band_by_genre(genre: GenreURLChoices) -> list[dict]:
+    bands = [band for band in BANDS if band['genre'].lower() == genre.value]
+    return bands
